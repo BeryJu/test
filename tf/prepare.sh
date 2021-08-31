26ebdf1e9c5f58df829e7a3676d242df7be5486b
Jens Langhammer <jens.langhammer@beryju.org>
2021-08-31 14:22:15 +0200

    set https_proxy in script

--- tf/prepare.sh
+++ tf/prepare.sh
@@ -3,3 +3,4 @@ FILE="ssh-key"
 echo $SSH_KEY | base64 -d > $FILE
 chmod 600 $FILE
 ssh -i $FILE -o StrictHostKeyChecking=no -D 1337 -q -C -N test@163.123.195.113 &
+export https_proxy=socks5://127.0.0.1:1337
